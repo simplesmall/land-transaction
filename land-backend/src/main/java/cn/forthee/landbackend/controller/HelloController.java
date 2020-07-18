3a6aa22a949b6680bf5cd64b694e75a8daf95eb3
guanshaochun <guanshaochun@ebupt.com>
2020-07-18 22:50:03 +0800

    First demo test get/set from front to backend use axios

--- land-backend/src/main/java/cn/forthee/landbackend/controller/HelloController.java
+++ land-backend/src/main/java/cn/forthee/landbackend/controller/HelloController.java
@@ -1,8 +1,12 @@
 package cn.forthee.landbackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
 
 /**
  * @author 简小
@@ -11,8 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/test")
+    String name = "张家大小姐,黄花大闺女";
+    @GetMapping("/get")
     public String helloTest(){
-        return "Hello world";
+        return this.name;
+    }
+    @PostMapping("/set")
+    public String setName(HttpServletResponse response, @PathParam("name") String Name){
+
+        return  "this.name"+this.name+"=>"+Name;
     }
 }
--- land-front/src/components/Login.vue
+++ land-front/src/components/Login.vue
@@ -2,6 +2,7 @@
   <div class="login">
     <h1>土地交易系统前端</h1>
     <input type="button" value="获取数据" @click="getData">
+    <input type="button" value="更改数据" @click="setData">
     {{msg}}
   </div>
 </template>
@@ -11,15 +12,24 @@
     name: 'Login',
     data() {
       return {
-        msg: null
+        msg: null,
+        name:'来自前端的数据'
       }
     },
     methods: {
       getData() {
-        this.$axios.get('/test').then((response) => {
+        this.$axios.get('/get').then((response) => {
           this.msg = response.data
         }).catch((error) => {
           console.log("出错咯~~~" + error)
+        })
+      },
+      setData(){
+        this.$axios.post('/set',"name=简简单单才是真").then(response=>{
+          console.log(response)
+          this.msg = response.data.name
+        }).catch(error=>{
+          console.log(error)
         })
       }
     },
